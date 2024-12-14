cb009b2338317e2791cf5ddadbb7acbe9d259985
mwthii <zmqt@proton.me>
2024-12-14 18:07:23 -0500

    Add files via upload

---
+++ extensions-without-signatures-system32.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem -Recurse C:\Windows\System32\*.exe,*.dll,*.jar,*.bat,*.reg -ea SilentlyContinue| ForEach-object {Get-AuthenticodeSignature $_ -ea SilentlyContinue} | Where-Object {$_.status -ine "Valid"}|Select Status,Path
