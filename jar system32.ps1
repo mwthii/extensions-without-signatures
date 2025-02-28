969b0d71aa529a93546e2c118ece451fd4ffc1ec
mwthii <zmqt@proton.me>
2025-02-28 02:07:43 -0500

    Delete jar system32.ps1

--- jar system32.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem -Recurse C:\Windows\System32\*.jar -ea SilentlyContinue| ForEach-object {Get-AuthenticodeSignature $_ -ea SilentlyContinue} | Where-Object {$_.status -ine "Valid"}|Select Status,Path
