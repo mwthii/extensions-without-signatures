d82b24b7f90003a5c56b34566c3cb69426613680
mwthii <zmqt@proton.me>
2025-02-28 02:07:05 -0500

    Delete allal-system32.ps1

--- allal-system32.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem -Recurse C:\Windows\System32\*.exe,*.dll,*.jar,*.bat,*.reg -ea SilentlyContinue| ForEach-object {Get-AuthenticodeSignature $_ -ea SilentlyContinue} | Where-Object {$_.status -ine "Valid"}|Select Status,Path
